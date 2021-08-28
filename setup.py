a4e8bdcc48ede9e3102bcf40dc484fcc93223a78
vermavinay982 <vermavinay982@gmail.com>
2021-08-28 13:46:46 +0530

    Documentation Updated

--- setup.py
+++ setup.py
@@ -3,18 +3,21 @@ from setuptools import _install_setup_requires, setup, find_packages
 with open('readme.md','r') as f:
 	long_desc = f.read()
 
+with open('short_desc.txt','r') as f:
+	short_desc = f.read()
+
 with open('requirements.txt','r') as f:
 	requirements = f.read()
-
 
 requirements = requirements.split('\n')
 
 setup(
 	name='vigenpy',
-	version='0.1.0',
-	description=long_desc,
+	version='0.1.3',
+	description=short_desc,
 	license='Proprietary',
 	long_description=long_desc,
+	long_description_content_type='text/markdown',
 	author='Vinay Verma',
 	maintainer='Vinay Verma',
 	maintainer_email='vermavinay982@gmail.com',
